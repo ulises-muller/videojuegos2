adbf162a9d54a59c2d9aeac80177968779ac95dc
ulises-muller <ulisesmuller667@gmail.com>
2024-10-18 19:29:43 -0300

    Revert "pausa"
    This reverts commit bced4dbf182e8a5e5a8deb02b09439f374ea2c57.

--- Assets/Scripts/addPoints.cs
+++ Assets/Scripts/addPoints.cs
@@ -2,15 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class addpoints : MonoBehaviour
+
+public class AddPoints : MonoBehaviour
 {
-    public int points = 10;
+    public int pointsToAdd;
+
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if(other.CompareTag("Player"))
         {
-            PointSytem.Instance.AddingPoints(points);
-            Destroy(gameObject);
+            pointSystem.Instance.addPoints(pointsToAdd);
         }
     }
 }
